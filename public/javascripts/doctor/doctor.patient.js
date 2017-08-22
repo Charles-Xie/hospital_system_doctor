3922632bfe10727c793cdbfc9118d606f2efbefc
Charles-Xie <15900635013@163.com>
2017-08-22 17:15:08 +0800

    add doctor.patient.init

--- public/javascripts/doctor/doctor.patient.js
+++ public/javascripts/doctor/doctor.patient.js
@@ -4,8 +4,23 @@ doctor.patient = (function() {
     // init the patient template
     var init = function(container) {
         $container = container;
-        $container.find('a.patient');
-        // find all the patient item on the list
+        $patientItem = $container.find('#patient-list-start');
+
+        var countItemNum = 0;
+        var $patientPage = $container.find('#patient1');
+        while($patientItem.next().length != 0) {
+            $patientItem = $patientItem.next('li.patient-item');
+            countItemNum += 1;
+            $patientItem.children('a.nav-link').first().attr('href', '#patient' + countItemNum);
+            // make the patient list item point to that patient page
+
+            //initialize the patient page
+            if(countItemNum != 1) {
+                $patientPage.parent().append(
+                    $patientPage.clone().attr('id', 'patient' + countItemNum)
+                );
+            }
+        }
     };
 
     var addPatientListener = function() {
